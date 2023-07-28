732dc193322357a85103a574399a6cf1f53b9d8a
primeOmar <omarbusolo@gmail.com>
2023-07-28 15:58:49 -0700

    changes/ get precision

--- get_flags.c
+++ get_flags.c
@@ -7,20 +7,20 @@
  */
 int get_flags(const char *format, int *i)
 {
-	int q, curr_i;
+	int i, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (q = 0; FLAGS_CH[q] != '\0'; q++)
-			if (format[curr_i] == FLAGS_CH[q])
+		for (q = 0; FLAGS_CH[i] != '\0'; ++i)
+			if (format[curr_i] == FLAGS_CH[i])
 			{
-				flags |= FLAGS_ARR[q];
+				flags |= FLAGS_ARR[i];
 				break;
 			}
-		if (FLAGS_CH[q] == 0)
+		if (FLAGS_CH[i] == 0)
 			break;
 	}
 	*i = curr_i - 1;
