3e3f1b22fa60812afcc75fc95d2f69bb7143c4ec
Sakinafarah <sakinafarah640@gmail.com>
2023-07-28 15:35:30 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+int (*check_specifier(const char*))(va_list);
+
+typedef struct func
+{
+	char *t;
+	int (*f)(valist);
+} func_t;
+int print_char(va_list);
+int print_str(va_list);
+int print_int(va_list);
+int print_cent(va_list);
+int print_dec(va_list);
 
 #endif
